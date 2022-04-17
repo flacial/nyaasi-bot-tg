7ab578f9cfab03403f2661091a3cc9aebe0e18a4
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-04-18 01:49:22 +0400

    feat(utils): Create telegramHTML

---
+++ src/utils/telegramHTML.util.ts
@@ -0,0 +1,3 @@
+export const strongHTML = (text: string) => `<strong>${text}</strong>`
+export const codeHTML = (text: string) => `<code>${text}</code>`
+export const linkHTML = (link: string) => `<a href="${link}">Link</a>`
