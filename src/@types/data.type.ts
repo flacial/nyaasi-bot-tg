d6e936ff76b766b15209a76d7f9f7c0ac9fd18d6
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-04-18 01:45:34 +0400

    feat: Create project types

---
+++ src/@types/data.type.ts
@@ -0,0 +1,30 @@
+import { Item } from "rss-parser"
+
+export type Anime = {
+    slug: string
+    description: string
+    status: 'finished' | 'continue'
+    episodeCounter: number
+    titles: {
+        en: string
+        en_jp: string
+        ja_jpp: string
+    }
+    posterImage: {
+        original: string
+    }
+}
+
+export interface FeedItem extends Item {
+    guid: string,
+    categoryId: string,
+    hash: string,
+    category: string,
+    size: string
+}
+
+export enum ALLOWED_CATEGORIES {
+    translatedAnime = '1_2'
+}
+
+export type State = Promise<{ x: FeedItem[] | FeedItem; lastStoredFeedItemIndex: number } | null>
