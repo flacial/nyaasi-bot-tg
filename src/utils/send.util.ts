7ab180f7573c9a1808944a1d7832b7441a6b1a0f
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-04-18 01:48:14 +0400

    feat(utils): Create send

---
+++ src/utils/send.util.ts
@@ -0,0 +1,28 @@
+import { FeedItem, ALLOWED_CATEGORIES, State } from "../@types/data.type"
+import { sendPhotoWithCaption } from "./sendPhotoWithCaption.util"
+
+export const sendReleases = async (feedItems: FeedItem[], i = feedItems.length - 1) => {
+    if (i < 0) return
+
+    const feedItem = feedItems[i]
+
+    if (feedItem.categoryId === ALLOWED_CATEGORIES.translatedAnime) await sendPhotoWithCaption(feedItem)
+
+    i -= 1
+
+    setTimeout(() => sendReleases(feedItems, i), 2000)
+}
+
+export const sendRelease = async (state: State) => {
+    if (!(await state)) return 'No new release'
+
+    const { x, lastStoredFeedItemIndex } = await state
+    const lateRelease = Array.isArray(x)
+    const startingIndex = lastStoredFeedItemIndex < 0
+        ? lateRelease && x.length
+        : lastStoredFeedItemIndex - 1
+
+    lateRelease ? await sendReleases(x, startingIndex) : await sendPhotoWithCaption(x)
+
+    return { ...state }
+}
