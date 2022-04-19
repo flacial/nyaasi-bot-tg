dfe6a63f6dca73947e46bea97f9836ec43a4213c
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-04-19 07:25:17 +0400

    fix: Unhandled release older than 50 releases

--- src/utils/validate.util.ts
+++ src/utils/validate.util.ts
@@ -4,23 +4,28 @@ import { storage } from "./storageInstance.util"
 
 export const validateFeed = async (getItems: Promise<FeedItem[]>) => {
     const feedItems = await getItems
+    
     const newFeedItem = await getItem(feedItems)
+    if (!newFeedItem) return null
 
     const hashes = await storage.toArrayByKeys()
     const lastStoredHash = hashes[hashes.length - 1]
 
-    const isNewRelease = newFeedItem.hash !== lastStoredHash
-    if (!isNewRelease) return null
+    const oldRelease = newFeedItem.hash === lastStoredHash
+    if (oldRelease) return null
 
-    const lastStoredFeedItemIndex = feedItems.findIndex(item => item.hash === lastStoredHash)
-    const isNotLastFeedItem = lastStoredFeedItemIndex > 0
+    const storedFeedItemIndex = feedItems.findIndex(item => item.hash === lastStoredHash)
+    
+    const lastFeedItem = storedFeedItemIndex === 0 
+    const feedItemNotFound = storedFeedItemIndex === -1
+    const manyFeedItems = storedFeedItemIndex > 0
 
-    if (newFeedItem.categoryId !== ALLOWED_CATEGORIES.translatedAnime && !isNotLastFeedItem) {
+    if (newFeedItem.categoryId !== ALLOWED_CATEGORIES.translatedAnime && lastFeedItem) {
         return null
     }
 
     return {
-        x: isNotLastFeedItem ? feedItems : newFeedItem,
-        lastStoredFeedItemIndex
+        x:  (feedItemNotFound || manyFeedItems) ? feedItems : newFeedItem,
+        storedFeedItemIndex
     }
 }
