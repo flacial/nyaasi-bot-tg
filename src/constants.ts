26b5c25107e1ca9d6af4f3cade64436edc47c4d3
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-04-24 02:31:54 +0400

    feat(constants): Create MAGNET_ERROR

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const NYAASI_RSS = 'https://nyaa.si/?page=rss'
 export const MAGNET_REDIRECT = 'https://magnet.flacial.dev/api/link/'
+export const MAGNET_ERROR = 'Oops, our apps is faster than their server. We could not get the magnet'
