9b7d6488f988a302a87fc7658e04b95539dd734e
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-06-11 07:49:52 +0400

    feat: Check for missing envs

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 if (!process.env.BOT_TOKEN) throw new Error('Must add a BOT_TOKEN to the .env file for the bot to work')
 if (!process.env.CHANNEL_ID) throw new Error('Must add a CHANNEL_ID to the .env file for the bot to work')
+if (!process.env.ACCESS_SECRET) throw new Error('Must add a ACCESS_SECRET to the .env file for the bot to work')
+if (!process.env.DATABASE_URL) throw new Error('Must add a DATABASE_URL to the .env file for the bot to work')
 if (!process.env.DEV_CHANNEL_ID)
     throw new Error('Must add a DEV_CHANNEL_ID to the .env file for the bot to work. Even if it is invalid')
 
